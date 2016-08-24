12b7ab9c87e43c328bedc5c715784adb2a9dc926
tastycode <kultiv8tor@gmail.com>
2016-08-24 07:36:03 -0700

    Rewrite: No more webpack, or build pipeline. Just HTML/JS/CSS

---
+++ victims.js
@@ -0,0 +1,558 @@
+[
+    {
+        "image": "160613.jpg",
+        "name": "Tyler Gebhard"
+    },
+    {
+        "image": "160611.jpg",
+        "name": "Andre Johnson"
+    },
+    {
+        "image": "160610.jpg",
+        "name": "Alva Braziel"
+    },
+    {
+        "image": "160606.jpg",
+        "name": "Micah Xavier Johnson"
+    },
+    {
+        "image": "160600.jpg",
+        "name": "Philando Castile"
+    },
+    {
+        "image": "160595.jpg",
+        "name": "Alton Sterling"
+    },
+    {
+        "image": "160590.jpg",
+        "name": "Delrawn Small"
+    },
+    {
+        "image": "160588.jpg",
+        "name": "Carlos Brown"
+    },
+    {
+        "image": "160587.jpg",
+        "name": "Jai Lateef Solveig Williams"
+    },
+    {
+        "image": "160577.jpg",
+        "name": "Tyrone Reado"
+    },
+    {
+        "image": "160564.jpg",
+        "name": "Germichael Kennedy"
+    },
+    {
+        "image": "160563.jpg",
+        "name": "Donte L. Johnson"
+    },
+    {
+        "image": "160556.jpg",
+        "name": "Angelo Brown"
+    },
+    {
+        "image": "160555.jpg",
+        "name": "Ismael Miranda"
+    },
+    {
+        "image": "160553.jpg",
+        "name": "Jay Anderson"
+    },
+    {
+        "image": "160552.jpg",
+        "name": "Deravis Caine Rogers"
+    },
+    {
+        "image": "160550.jpg",
+        "name": "Antonio Glenn Richardson"
+    },
+    {
+        "image": "160549.jpg",
+        "name": "Quencezola Maurice Splunge"
+    },
+    {
+        "image": "160547.jpg",
+        "name": "Isaiah Core III"
+    },
+    {
+        "image": "160539.jpg",
+        "name": "Lorez Chambers"
+    },
+    {
+        "image": "160538.jpg",
+        "name": "Clarence Mahogany X Howard"
+    },
+    {
+        "image": "160537.jpg",
+        "name": "Antwun Shumpert"
+    },
+    {
+        "image": "160534.jpg",
+        "name": "Rashaun Lloyd"
+    },
+    {
+        "image": "160521.jpg",
+        "name": "Michael Moore"
+    },
+    {
+        "image": "160517.jpg",
+        "name": "John Williams"
+    },
+    {
+        "image": "0533.jpg",
+        "name": "Keith Bursey"
+    },
+    {
+        "image": "160506.jpg",
+        "name": "John Michael Brisco"
+    },
+    {
+        "image": "160497.jpg",
+        "name": "Willis N. Walker Jr."
+    },
+    {
+        "image": "160495.jpg",
+        "name": "Henry Green V"
+    },
+    {
+        "image": "160489.jpg",
+        "name": "Demarco Rhymes"
+    },
+    {
+        "image": "160487.jpg",
+        "name": "Rodney Rodriguez Smith"
+    },
+    {
+        "image": "160485.jpg",
+        "name": "Willie Demetrius James"
+    },
+    {
+        "image": "160484.jpg",
+        "name": "Michael Johnson"
+    },
+    {
+        "image": "160477.jpg",
+        "name": "Osee Calix"
+    },
+    {
+        "image": "0485.jpg",
+        "name": "Ollie Lee Brooks"
+    },
+    {
+        "image": "160461.jpg",
+        "name": "Terry Frost III"
+    },
+    {
+        "image": "160459.jpg",
+        "name": "Doll Pierre-Louis"
+    },
+    {
+        "image": "160455.jpg",
+        "name": "Vernell Charles Bing Jr."
+    },
+    {
+        "image": "160450.jpg",
+        "name": "Michael Eugene Wilson Jr."
+    },
+    {
+        "image": "160444.jpg",
+        "name": "Joshua S. Beebee"
+    },
+    {
+        "image": "160441.jpg",
+        "name": "Kentrill William Carraway"
+    },
+    {
+        "image": "160440.jpg",
+        "name": "Jessica Williams"
+    },
+    {
+        "image": "160438.jpg",
+        "name": "Reginald Darnell Dogan"
+    },
+    {
+        "image": "160430.jpg",
+        "name": "Jabril Robinson"
+    },
+    {
+        "image": "160423.jpg",
+        "name": "James Craig Simpson"
+    },
+    {
+        "image": "0426.jpg",
+        "name": "David Dementarius Canty"
+    },
+    {
+        "image": "160418.jpg",
+        "name": "Sean Ryan Mondragon"
+    },
+    {
+        "image": "160413.jpg",
+        "name": "Arthur J. DaRosa"
+    },
+    {
+        "image": "160409.jpg",
+        "name": "Arthur R. Williams Jr."
+    },
+    {
+        "image": "160407.jpg",
+        "name": "Willie Tillman"
+    },
+    {
+        "image": "160405.jpg",
+        "name": "Jaffort Demont Smith"
+    },
+    {
+        "image": "160401.jpg",
+        "name": "Lionel Gibson"
+    },
+    {
+        "image": "160399.jpg",
+        "name": "Alton Witchard"
+    },
+    {
+        "image": "160398.jpg",
+        "name": "Ronald D. Williams Jr."
+    },
+    {
+        "image": "160397.jpg",
+        "name": "Burt T. Johnson"
+    },
+    {
+        "image": "160395.jpg",
+        "name": "Deresha Armstrong"
+    },
+    {
+        "image": "160385.jpg",
+        "name": "Benston Calvin Clinkscales"
+    },
+    {
+        "image": "160383.jpg",
+        "name": "Charlin Charles"
+    },
+    {
+        "image": "160374.jpg",
+        "name": "Ashtian Duane Barnes"
+    },
+    {
+        "image": "0367.jpg",
+        "name": "Joshua Brooks"
+    },
+    {
+        "image": "160361.jpg",
+        "name": "Demarcus Semer"
+    },
+    {
+        "image": "160350.jpg",
+        "name": "Jorevis Scruggs"
+    },
+    {
+        "image": "160348.jpg",
+        "name": "Rico Don Rae Johnson"
+    },
+    {
+        "image": "160347.jpg",
+        "name": "Demetrius Dorsey"
+    },
+    {
+        "image": "160346.jpg",
+        "name": "Edson Thevenin"
+    },
+    {
+        "image": "160344.jpg",
+        "name": "Richard Bard Jr."
+    },
+    {
+        "image": "160343.jpg",
+        "name": "Kisha Arrone"
+    },
+    {
+        "image": "160342.jpg",
+        "name": "George Tillman"
+    },
+    {
+        "image": "0446.jpg",
+        "name": "Bennie D. Bennett"
+    },
+    {
+        "image": "160337.jpg",
+        "name": "Robert Jerome Howard"
+    },
+    {
+        "image": "160332.jpg",
+        "name": "Rodney Watts"
+    },
+    {
+        "image": "160328.jpg",
+        "name": "Pierre L. Loury"
+    },
+    {
+        "image": "160326.jpg",
+        "name": "Jerand Edward Ross"
+    },
+    {
+        "image": "160321.jpg",
+        "name": "Quron Williams"
+    },
+    {
+        "image": "160319.jpg",
+        "name": "Diahlo Grant"
+    },
+    {
+        "image": "160318.jpg",
+        "name": "Lamont Gulley"
+    },
+    {
+        "image": "160317.jpg",
+        "name": "Dazion Flenaugh"
+    },
+    {
+        "image": "160311.jpg",
+        "name": "Laronda Sweatt"
+    },
+    {
+        "image": "160307.jpg",
+        "name": "Kevin Hicks"
+    },
+    {
+        "image": "0315.jpg",
+        "name": "Darius Randell Robinson"
+    },
+    {
+        "image": "160299.jpg",
+        "name": "Cameron Glover"
+    },
+    {
+        "image": "0385.jpg",
+        "name": "Grant Hamilton"
+    },
+    {
+        "image": "0293.jpg",
+        "name": "Kimani Michael Johnson"
+    },
+    {
+        "image": "160294.jpg",
+        "name": "James Brown III"
+    },
+    {
+        "image": "160283.jpg",
+        "name": "Deriante Miller"
+    },
+    {
+        "image": "160278.jpg",
+        "name": "Dominique Silva"
+    },
+    {
+        "image": "160277.jpg",
+        "name": "Jermon J. Seals"
+    },
+    {
+        "image": "160272.jpg",
+        "name": "Alexio Allen"
+    },
+    {
+        "image": "160263.jpg",
+        "name": "Robert Dentmond"
+    },
+    {
+        "image": "160257.jpg",
+        "name": "Thurman Reynolds"
+    },
+    {
+        "image": "160255.jpg",
+        "name": "India Beaty"
+    },
+    {
+        "image": "160256.jpg",
+        "name": "Torrey Lamar Robinson"
+    },
+    {
+        "image": "160251.jpg",
+        "name": "Christopher Nelms"
+    },
+    {
+        "image": "160238.jpg",
+        "name": "Lamar Harris"
+    },
+    {
+        "image": "0256.jpg",
+        "name": "Kevin Higgenbotham"
+    },
+    {
+        "image": "0247.jpg",
+        "name": "Jacai Colson"
+    },
+    {
+        "image": "160231.jpg",
+        "name": "Keith Montgomery"
+    },
+    {
+        "image": "160230.jpg",
+        "name": "Peter Matthew Gaines"
+    },
+    {
+        "image": "160229.jpg",
+        "name": "Marco Antonio Loud"
+    },
+    {
+        "image": "160225.jpg",
+        "name": "Tyre Privott"
+    },
+    {
+        "image": "160222.jpg",
+        "name": "Arteair Porter"
+    },
+    {
+        "image": "160206.jpg",
+        "name": "Evan Ezekiel Joseph"
+    },
+    {
+        "image": "160193.jpg",
+        "name": "Kionte Desean Spencer"
+    },
+    {
+        "image": "160192.jpg",
+        "name": "Cedric Larry Ford"
+    },
+    {
+        "image": "160189.jpg",
+        "name": "Gregory Gunn"
+    },
+    {
+        "image": "160188.jpg",
+        "name": "Christopher J. Davis"
+    },
+    {
+        "image": "160182.jpg",
+        "name": "Travis Stevenson"
+    },
+    {
+        "image": "160178.jpg",
+        "name": "Thomas Lane"
+    },
+    {
+        "image": "160170.jpg",
+        "name": "Che Andre Taylor"
+    },
+    {
+        "image": "0167.jpg",
+        "name": "Marquintan Sandlin"
+    },
+    {
+        "image": "0180.jpg",
+        "name": "Ariel J. Clark"
+    },
+    {
+        "image": "160154.jpg",
+        "name": "Paul Gaston"
+    },
+    {
+        "image": "160144.jpg",
+        "name": "Calin Roquemore"
+    },
+    {
+        "image": "160141.jpg",
+        "name": "Calvin Smith"
+    },
+    {
+        "image": "160140.jpg",
+        "name": "Dyzhawn L. Perkins"
+    },
+    {
+        "image": "160139.jpg",
+        "name": "Ali Eisa Abdalla Yahia"
+    },
+    {
+        "image": "160134.jpg",
+        "name": "Sahlah Ridgeway"
+    },
+    {
+        "image": "160132.jpg",
+        "name": "Peter Fanfan"
+    },
+    {
+        "image": "160129.jpg",
+        "name": "Mohamed Barry"
+    },
+    {
+        "image": "160121.jpg",
+        "name": "Eric Harris"
+    },
+    {
+        "image": "160120.jpg",
+        "name": "David Joseph"
+    },
+    {
+        "image": "160119.jpg",
+        "name": "Shalamar Longer"
+    },
+    {
+        "image": "160117.jpg",
+        "name": "Marese Collins"
+    },
+    {
+        "image": "160104.jpg",
+        "name": "Antronie Scott"
+    },
+    {
+        "image": "160092.jpg",
+        "name": "Bruce Kelley Jr.160091.jpg, Charles M. Smith"
+    },
+    {
+        "image": "160085.jpg",
+        "name": "Christopher Michael Dew"
+    },
+    {
+        "image": "160084.jpg",
+        "name": "Randolph McClain"
+    },
+    {
+        "image": "160076.jpg",
+        "name": "Janet Wilson"
+    },
+    {
+        "image": "160072.jpg",
+        "name": "Johnathan Bratcher"
+    },
+    {
+        "image": "160061.jpg",
+        "name": "Cedric Lamont Norris"
+    },
+    {
+        "image": "160059.jpg",
+        "name": "Timothy R. Albert"
+    },
+    {
+        "image": "160046.jpg",
+        "name": "Crayton West"
+    },
+    {
+        "image": "160036.jpg",
+        "name": "Henry Bennett"
+    },
+    {
+        "image": "160034.jpg",
+        "name": "Rakeem O. Bentley"
+    },
+    {
+        "image": "0085.jpg",
+        "name": "Gynnya McMillen"
+    },
+    {
+        "image": "160021.jpg",
+        "name": "Lionel Waters"
+    },
+    {
+        "image": "160007.jpg",
+        "name": "Eric John Senegal"
+    },
+    {
+        "image": "160006.jpg",
+        "name": "Rodney Turner"
+    },
+    {
+        "image": "160005.jpg",
+        "name": "Germonta Wallac"
+    }
+]
